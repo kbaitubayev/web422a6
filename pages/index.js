341c8193c2b3f8bd905d2fc833de4491778ccbfd
kbaitubayev <kbaitubayev@myseneca.ca>
2022-12-02 23:24:43 -0500

    appended text to title on Home

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-<title>Create Next App (Starter Kaktus Site)</title>
+
 
 export default function Home() {
   return (
